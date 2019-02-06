93bf36b2db0323187acf837b87efe5de8837f4fb
Philipp Eschenbach <philipp@uberall.com>
2019-02-06 15:23:59 +0100

    make deployment a bit noisier

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl -s https://api.github.com/repos/peh/bamboohr-slack-bot/releases/latest | \
-    jq '.assets[] | select(.name = "current.war") |  .browser_download_url' --raw-output | \
-    xargs curl -L# --output /app/current.jar
+DOWNLOAD_URL=`curl -s https://api.github.com/repos/peh/bamboohr-slack-bot/releases/latest | jq '.assets[] | select(.name = "current.war") |  .browser_download_url' --raw-output`
+echo "Downloading from $DOWNLOAD_URL"
+curl -L# --output /app/current.jar $DOWNLOAD_URL
 
 java -jar /app/current.jar -Dredis.host=redis
