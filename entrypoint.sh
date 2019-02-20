b65d1ba3095b41b26055fc200a1e6a0a7b56eded
Philipp Eschenbach <philipp@uberall.com>
2019-02-20 11:33:50 +0100

    make sure FileWatch is disabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ DOWNLOAD_URL=`curl -s https://api.github.com/repos/peh/bamboohr-slack-bot/releas
 echo "Downloading from $DOWNLOAD_URL"
 curl -L# --output /app/current.jar $DOWNLOAD_URL
 
-java -jar /app/current.jar -Ddb.file=$DB_FILE
+java -jar /app/current.jar -Ddb.file=$DB_FILE -D-Dmicronaut.io.watch.enabled=false -Dmicronaut.io.watch.restart=false
